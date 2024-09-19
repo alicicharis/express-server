a920c5e3c2c959f3479c7757781394b14ce77eff
Haris Alicic <haris.alicic1@gmail.com>
2024-09-19 22:02:34 +0200

    updates

--- cert.sh
+++
@@ -1,9 +0,0 @@
-docker run -it --rm --name certbot \
-  -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
-  -v "$(pwd)/certbot/www:/var/www/certbot" \
-  certbot/certbot certonly --webroot \
-  --webroot-path=/var/www/certbot \
-  --register-unsafely-without-email \
-  --agree-tos \
-  --no-eff-email \
-  -d api.harislearn.com
