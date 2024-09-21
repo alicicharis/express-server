65b420fc4ed2063b042890c46e62efa5a907fde6
Haris Alicic <haris.alicic1@gmail.com>
2024-09-21 16:37:43 +0200

    updates

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,6 +2,8 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { db } from "./index";
 
 (async () => {
+  console.log("Migrating database...");
   await migrate(db, { migrationsFolder: "src/drizzle" });
+  console.log("Migrated");
   process.exit(0);
 })();
